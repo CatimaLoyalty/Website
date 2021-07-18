49006cf69e8774046253a586ed9e5aed542e8eb2
Sylvia van Os <sylvia@hackerchick.me>
2021-07-18 17:55:47 +0200

    Loop over all screenshots

--- init.sh
+++ init.sh
@@ -2,7 +2,22 @@
 
 # Images
 curl https://raw.githubusercontent.com/TheLastProject/Catima/master/app/src/main/ic_launcher-playstore.png --output assets/appicon.png
-curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-01.png --output assets/screenshot/yourscreenshot.png
+curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-01.png --output assets/screenshot/screenshot-01.png
+curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-02.png --output assets/screenshot/screenshot-02.png
+curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-03.png --output assets/screenshot/screenshot-03.png
+curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-04.png --output assets/screenshot/screenshot-04.png
+curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-05.png --output assets/screenshot/screenshot-05.png
+curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-06.png --output assets/screenshot/screenshot-06.png
+curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-07.png --output assets/screenshot/screenshot-07.png
+curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-08.png --output assets/screenshot/screenshot-08.png
 
 # Changelog
-curl https://raw.githubusercontent.com/TheLastProject/Catima/master/CHANGELOG.md --output _pages/changelog.md
+cat << EOF > _pages/changelog.md
+---
+layout: page
+title: What's New
+include_in_header: true
+---
+
+EOF
+curl https://raw.githubusercontent.com/TheLastProject/Catima/master/CHANGELOG.md >> _pages/changelog.md
