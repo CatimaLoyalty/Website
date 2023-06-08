83e28c77d4ed2fd6746c90348039231a6eb26d17
FC Stegerman <flx@obfusk.net>
2023-06-08 15:01:25 +0200

    update repo url

--- init.sh
+++ init.sh
@@ -2,15 +2,15 @@
 
 # Images
 mkdir -p assets/screenshot
-curl https://raw.githubusercontent.com/TheLastProject/Catima/master/app/src/main/ic_launcher-playstore.png --output assets/appicon.png
-curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-01.png --output assets/screenshot/screenshot-01.png
-curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-02.png --output assets/screenshot/screenshot-02.png
-curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-03.png --output assets/screenshot/screenshot-03.png
-curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-04.png --output assets/screenshot/screenshot-04.png
-curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-05.png --output assets/screenshot/screenshot-05.png
-curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-06.png --output assets/screenshot/screenshot-06.png
-curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-07.png --output assets/screenshot/screenshot-07.png
-curl https://raw.githubusercontent.com/TheLastProject/Catima/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-08.png --output assets/screenshot/screenshot-08.png
+curl https://raw.githubusercontent.com/CatimaLoyalty/Android/master/app/src/main/ic_launcher-playstore.png --output assets/appicon.png
+curl https://raw.githubusercontent.com/CatimaLoyalty/Android/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-01.png --output assets/screenshot/screenshot-01.png
+curl https://raw.githubusercontent.com/CatimaLoyalty/Android/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-02.png --output assets/screenshot/screenshot-02.png
+curl https://raw.githubusercontent.com/CatimaLoyalty/Android/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-03.png --output assets/screenshot/screenshot-03.png
+curl https://raw.githubusercontent.com/CatimaLoyalty/Android/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-04.png --output assets/screenshot/screenshot-04.png
+curl https://raw.githubusercontent.com/CatimaLoyalty/Android/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-05.png --output assets/screenshot/screenshot-05.png
+curl https://raw.githubusercontent.com/CatimaLoyalty/Android/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-06.png --output assets/screenshot/screenshot-06.png
+curl https://raw.githubusercontent.com/CatimaLoyalty/Android/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-07.png --output assets/screenshot/screenshot-07.png
+curl https://raw.githubusercontent.com/CatimaLoyalty/Android/master/fastlane/metadata/android/en-US/images/phoneScreenshots/screenshot-08.png --output assets/screenshot/screenshot-08.png
 
 # Changelog
 cat << EOF > _pages/changelog.md
@@ -22,4 +22,4 @@ include_in_footer: true
 ---
 
 EOF
-curl https://raw.githubusercontent.com/TheLastProject/Catima/master/CHANGELOG.md >> _pages/changelog.md
+curl https://raw.githubusercontent.com/CatimaLoyalty/Android/master/CHANGELOG.md >> _pages/changelog.md
